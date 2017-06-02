1de590c081db005909872f2b9a07032a3b3490e5
Davy Daenen <davydaenen3@hotmail.com>
2017-06-02 15:09:51 +0200

    controller functie

---
+++ src/TravelClaimClient/Models/LuggageClaimObjectCoverage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelClaimClient.Models
+{
+    public class LuggageClaimObjectCoverage
+    {
+        public string CarelessUseofLuggageClaimObject { get; set; }
+        public string DamagetoLodging { get; set; }
+        public string InsuredPersonLiableforLuggageClaimObject { get; set; }
+        public string LuggageClaimCause { get; set; }
+        public string LuggageClaimObjectinHandLuggage { get; set; }
+        public string LuggageClaimObjectLocation { get; set; }
+        public string LuggageClaimObjectSupervision { get; set; }
+        public string LuggageClaimObject { get; set; }
+        public string PolicyNumber { get; set; }
+    }
+}
